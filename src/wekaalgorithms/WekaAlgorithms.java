9dd2afd2bca93f6d7344ead66160c5566c708ce5
Bruno Casali <brunoocasali@gmail.com>
2015-08-18 22:40:21 -0300

    added many changes in code, trying to fix predicitions now

--- src/wekaalgorithms/WekaAlgorithms.java
+++ src/wekaalgorithms/WekaAlgorithms.java
@@ -5,6 +5,13 @@
  */
 package wekaalgorithms;
 
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instances;
+import weka.core.Utils;
+import weka.core.converters.ConverterUtils.DataSource;
+
 /**
  *
  * @author bruno
@@ -14,8 +21,73 @@ public class WekaAlgorithms {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        DataSource data = new DataSource("src/letter.arff");
+        Instances ins = data.getDataSet();
+
+        NaiveBayes naive = new NaiveBayes();
+        ins.setClassIndex(16);
+        naive.buildClassifier(ins);
+
+        Evaluation eval = new Evaluation(ins);
+
+        printSummary(naive, eval, ins);
+//        System.out.println(ins.toString());
+//        
+//        
+//        System.out.println(naive.toString());
+//        
+//        Instance k = new Instance(5);
+//        k.setDataset(ins);
+//        k.setValue(0, "sunny");
+//        k.setValue(1, "mild");
+//        k.setValue(2, "normal");
+//        k.setValue(3, "FALSE");
+//        
+//        double prob[] = naive.distributionForInstance(k);
+//        System.out.println("YES: " + prob[0]);
+//        System.out.println("NO: " + prob[1]);
+
+        // loads data and set class index
     }
-    
+
+    private static void printSummary(Classifier base, Evaluation eval, Instances data) throws Exception {
+        // output evaluation
+        System.out.println();
+        System.out.println("=== Setup ===");
+//        System.out.println("Classifier: " + classifierName.getClass().getName() + " " + Utils.joinOptions(base.getOptions()));
+        System.out.println("Dataset: " + data.relationName());
+        System.out.println();
+
+        // output predictions
+        System.out.println("# - actual - predicted - error - distribution - token");
+        for (int i = 0; i < data.numInstances(); i++) {
+            double pred = base.classifyInstance(data.instance(i));
+            double actual = data.instance(i).classValue();
+            double[] dist = base.distributionForInstance(data.instance(i));
+
+            if (pred != actual) {
+                System.out.print((i + 1));
+                System.out.print(" - ");
+                System.out.print(data.instance(i).toString(data.classIndex()));
+                System.out.print(" - ");
+                System.out.print(data.classAttribute().value((int) pred));
+                System.out.print(" - ");
+                if (pred != data.instance(i).classValue()) {
+                    System.out.print("yes");
+                } else {
+                    System.out.print("no");
+                }
+                System.out.print(" - ");
+                System.out.print(Utils.arrayToString(dist));
+                System.out.print(" - ");
+                data.instance(i).enumerateAttributes().toString();
+                System.out.println();
+            }
+        }
+
+        System.out.println(eval.toSummaryString());
+        System.out.println(eval.toClassDetailsString());
+        System.out.println(eval.toMatrixString());
+    }
 }
