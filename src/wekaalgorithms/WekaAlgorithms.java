d345e5656169e694b22a8b371bcf43530cff7480
Bruno Casali <brunoocasali@gmail.com>
2015-08-19 02:33:09 -0300

    Ready to use! fixed problems, working now, just run!

--- src/wekaalgorithms/WekaAlgorithms.java
+++ src/wekaalgorithms/WekaAlgorithms.java
@@ -1,10 +1,29 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2015 brunoocasali.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package wekaalgorithms;
 
+import java.util.Random;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
@@ -14,80 +33,35 @@ import weka.core.converters.ConverterUtils.DataSource;
 
 /**
  *
- * @author bruno
+ * @author brunoocasali
  */
 public class WekaAlgorithms {
 
     /**
      * @param args the command line arguments
+     * @throws java.lang.Exception
      */
     public static void main(String[] args) throws Exception {
-        DataSource data = new DataSource("src/letter.arff");
-        Instances ins = data.getDataSet();
+        DataSource source = new DataSource("src/files/letter.arff");
 
-        NaiveBayes naive = new NaiveBayes();
-        ins.setClassIndex(16);
-        naive.buildClassifier(ins);
+        int folds = 10;
+        int runs = 30;
 
-        Evaluation eval = new Evaluation(ins);
+        Classifier cls = new NaiveBayes();
+        Instances data = source.getDataSet();
+        data.setClassIndex(16);
 
-        printSummary(naive, eval, ins);
-//        System.out.println(ins.toString());
-//        
-//        
-//        System.out.println(naive.toString());
-//        
-//        Instance k = new Instance(5);
-//        k.setDataset(ins);
-//        k.setValue(0, "sunny");
-//        k.setValue(1, "mild");
-//        k.setValue(2, "normal");
-//        k.setValue(3, "FALSE");
-//        
-//        double prob[] = naive.distributionForInstance(k);
-//        System.out.println("YES: " + prob[0]);
-//        System.out.println("NO: " + prob[1]);
+        System.out.println("#seed \t correctly instances \t percentage of corrects\n");
+        for (int i = 1; i <= runs; i++) {
+            Evaluation eval = new Evaluation(data);
+            eval.crossValidateModel(cls, data, folds, new Random(i));
 
-        // loads data and set class index
-    }
-
-    private static void printSummary(Classifier base, Evaluation eval, Instances data) throws Exception {
-        // output evaluation
-        System.out.println();
-        System.out.println("=== Setup ===");
-//        System.out.println("Classifier: " + classifierName.getClass().getName() + " " + Utils.joinOptions(base.getOptions()));
-        System.out.println("Dataset: " + data.relationName());
-        System.out.println();
-
-        // output predictions
-        System.out.println("# - actual - predicted - error - distribution - token");
-        for (int i = 0; i < data.numInstances(); i++) {
-            double pred = base.classifyInstance(data.instance(i));
-            double actual = data.instance(i).classValue();
-            double[] dist = base.distributionForInstance(data.instance(i));
-
-            if (pred != actual) {
-                System.out.print((i + 1));
-                System.out.print(" - ");
-                System.out.print(data.instance(i).toString(data.classIndex()));
-                System.out.print(" - ");
-                System.out.print(data.classAttribute().value((int) pred));
-                System.out.print(" - ");
-                if (pred != data.instance(i).classValue()) {
-                    System.out.print("yes");
-                } else {
-                    System.out.print("no");
-                }
-                System.out.print(" - ");
-                System.out.print(Utils.arrayToString(dist));
-                System.out.print(" - ");
-                data.instance(i).enumerateAttributes().toString();
-                System.out.println();
-            }
+            System.out.println("#" + i + "\t" + summary(eval));
         }
-
-        System.out.println(eval.toSummaryString());
-        System.out.println(eval.toClassDetailsString());
-        System.out.println(eval.toMatrixString());
+    }
+    
+    private static String summary(Evaluation eval){
+        return Utils.doubleToString(eval.correct(), 12, 4) + "\t " +
+                Utils.doubleToString(eval.pctCorrect(), 12, 4) + "%";
     }
 }
